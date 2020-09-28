453f22d1841216f0c67e795f4325e2dd6114e40e
Roshani Ayu Pranasti <asti.roshani@gmail.com>
2020-09-28 22:15:37 +0700

    initial commit

---
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/roshaniayu/diarypokedex/model/Pokemon.kt
@@ -0,0 +1,6 @@
+package id.ac.ui.cs.mobileprogramming.roshaniayu.diarypokedex.model
+
+class Pokemon {
+    var name: String? = null
+    var url: String? = null
+}
---
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/roshaniayu/diarypokedex/model/PokemonResponse.kt
@@ -0,0 +1,8 @@
+package id.ac.ui.cs.mobileprogramming.roshaniayu.diarypokedex.model
+
+class PokemonResponse {
+    var count = 0
+    var next: String? = null
+    var previous: Any? = null
+    var results: List<Pokemon>? = null
+}
