9872fac36014553a0a4f9e282d402673a15d695b
Roshani Ayu Pranasti <asti.roshani@gmail.com>
2020-12-25 23:34:47 +0700

    implement connectivity manager to check connection availability

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/roshaniayu/pokediary/SplashActivity.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/roshaniayu/pokediary/SplashActivity.kt
@@ -1,13 +1,20 @@
 package id.ac.ui.cs.mobileprogramming.roshaniayu.pokediary
 
 import MyGLSurfaceView
+import android.Manifest
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
 import android.opengl.GLSurfaceView
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 
 class SplashActivity : Activity() {
 
@@ -20,10 +27,26 @@ class SplashActivity : Activity() {
         // as the ContentView for this Activity.
         gLView = MyGLSurfaceView(this)
         setContentView(gLView)
-        // simulate some async initialization like authenticating user from network
-        Handler(Looper.getMainLooper()).postDelayed({
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
-        }, 2000)
+
+        if (isOnline()) {
+            Handler(Looper.getMainLooper()).postDelayed({
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            }, 2000)
+        } else {
+            AlertDialog.Builder(this, R.style.AlertDialogCustom)
+                .setMessage("You need to have an internet connection to open PokéDiary")
+                .setPositiveButton("OK") { _, _ ->
+                    super.onBackPressed()
+                    finish()
+                }
+                .show()
+        }
+    }
+
+    private fun isOnline(): Boolean {
+        val connMgr = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkInfo: NetworkInfo? = connMgr.activeNetworkInfo
+        return networkInfo?.isConnected == true
     }
 }
